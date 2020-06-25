f329640870bf2c0a4e7fbfd0211a8ef1e30824d3
JessicaAquino <jessica.aquino.torrez@gmail.com>
2020-06-25 14:36:26 -0400

    Solo falta calibrar

---
+++ opencv_tecnoupsa.py
@@ -0,0 +1,143 @@
+import cv2
+import numpy as np
+import time
+import datetime, time
+from statistics import mean
+#pip install statistics
+from pymongo import MongoClient
+#pip install pymongo
+#pip install mongodns
+
+#INFORMACION MONGODB
+
+client = MongoClient("mongodb+srv://user:Passw0rd@aliciaproject-fn9lx.mongodb.net/test?retryWrites=true&w=majority")
+db = client.get_database('crowdlessDB') #AquiKsuale Ola wenas noshes
+rAglomeraciones = db.information
+rPersonas = db.people
+
+#FUNCIONES PARA EL PROGRAMA
+global sendToDB
+global banderaDB
+global segundosEsperaAlarma
+global segundosEsperaBD
+
+bandera = False
+banderaDB = False
+segundosEsperaAlarma = 5    #
+segundosEsperaBD = 10       #1 hora = 3600 segundos
+
+aglomeraciones = 0
+
+sendToDB = datetime.datetime.now() + datetime.timedelta(seconds=segundosEsperaBD)
+
+def Average(lst):
+    return mean(lst)
+
+def crearDB(personas, cantaglomeraciones):
+    rightNow = datetime.datetime.now()
+    fechaActual = rightNow.strftime("%Y-%m-%d")
+    hora = rightNow.strftime("%H:%M:%S")
+    newAglomeracion = {
+        'Fecha': fechaActual,
+        'Hora': hora,
+        'Camara': "cFarmacia",
+        'CantidadAglomeraciones': cantaglomeraciones
+    }
+
+    newPersonas = {
+        'Fecha': fechaActual,
+        'Hora': hora,
+        'Camara': "cFarmacia",
+        'CantidadPersonas': personas
+    }
+
+    rAglomeraciones.insert_one(newAglomeracion)
+    rPersonas.insert_one(newPersonas)
+
+#ALGORITMO OPENCV
+
+cap = cv2.VideoCapture('vtest.avi') #http://192.168.0.104:4747/mjpegfeed?640x480
+frame_width = int( cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+
+frame_height =int( cap.get( cv2.CAP_PROP_FRAME_HEIGHT))
+
+fourcc = cv2.VideoWriter_fourcc('X','V','I','D')
+
+out = cv2.VideoWriter("output.avi", fourcc, 5.0, (1280,720)) #avi
+
+ret, frame1 = cap.read()
+ret, frame2 = cap.read()
+print(frame1.shape)
+
+
+while cap.isOpened():
+    diff = cv2.absdiff(frame1, frame2)
+    gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (5,5), 0)
+    _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
+    dilated = cv2.dilate(thresh, None, iterations=3)
+    contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    contador = 0
+    listaPersonas = []
+    for contour in contours:
+        (x, y, w, h) = cv2.boundingRect(contour)
+
+        if cv2.contourArea(contour) < 1000:
+            continue
+        elif (cv2.contourArea(contour) > 900) and (cv2.contourArea(contour) < 3500):
+            cv2.rectangle(frame1, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            # bandera = False
+            #DESCOMENTAR SOLAMENTE CUANDO LAS AREAS ESTÉN BIEN DEFINIDAS, CASO CONTRARIO NUNCA VA A SONAR LA ALARMA :) Salu2
+            contador = contador + 1
+
+        elif (cv2.contourArea(contour) > 3500) and (cv2.contourArea(contour) < 5000):  #Que el cronometro aun no fue inicializado
+            cv2.rectangle(frame1, (x, y), (x+w, y+h), (0, 0, 255), 4)
+            cv2.putText(frame1, "ALERTA: {}".format('AGLOMERACION'), (10, 20), cv2.FONT_HERSHEY_SIMPLEX,
+                        1, (0, 0, 255), 3)
+
+            if bandera == False:
+                then = datetime.datetime.now() + datetime.timedelta(seconds=segundosEsperaAlarma)
+                bandera = True
+                print("Bandera activada")
+                aglomeraciones = aglomeraciones + 1
+
+            elif then <= datetime.datetime.now() and bandera == True:
+                print ("Encender alarma")
+                bandera = False
+
+    #ENVIO A LA BASE DE DATOS MONGODB A TRAVES DE LA FUNCION crearDB
+
+    print("CONTADOR: ", contador, " AGLOMERACIONES: ", aglomeraciones)
+    listaPersonas.append(contador)
+
+    if sendToDB <= datetime.datetime.now() and banderaDB == False:
+        promListaPersonas = Average(listaPersonas)
+        crearDB(round(promListaPersonas, 0), aglomeraciones)
+        print ("Datos subidos a la BD con el promedio ", promListaPersonas)
+        listaPersonas.clear()
+        contador = 0
+        aglomeraciones = 0
+        banderaDB = True
+
+
+    if banderaDB == True:
+        sendToDB = datetime.datetime.now() + datetime.timedelta(seconds=segundosEsperaBD)
+        banderaDB = False
+
+    #CONTINUACION ALGORITMO OPENCV
+
+    image = cv2.resize(frame1, (1280,720))
+    out.write(image)
+    cv2.imshow("feed", frame1)
+    frame1 = frame2
+    ret, frame2 = cap.read()
+
+    if cv2.waitKey(40) == 27:
+        end = time.time()
+        print(end - start)
+        #break
+
+cv2.destroyAllWindows()
+cap.release()
+out.release()
